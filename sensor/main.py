a941e93107ac70ff745c7cb834fc3a0adb539d1d
Fábio Maia <fabiu.maia@gmail.com>
2018-04-07 17:00:07 +0100

    Cleanup sensor script a bit

--- sensor/main.py
+++ sensor/main.py
@@ -11,27 +11,27 @@ from kafka import KafkaProducer
 
 def findCoordinates(lat, longit, route_range):
     track_points = []
-    numberOfPoints = 10;
-    degreesPerPoint = 360 / numberOfPoints;
+    numberOfPoints = 10
+    degreesPerPoint = 360 / numberOfPoints
 
     # Keep track of the angle from centre to radius
-    currentAngle = 0;
+    currentAngle = 0
 
     # The points on the radius will be lat+x2, long+y2
     # Track the points we generate to return at the end
     for i in range(0,numberOfPoints):
         # X2 point will be cosine of angle * radius (range)
-        x2 = math.cos(currentAngle) * route_range ;
+        x2 = math.cos(currentAngle) * route_range
         # Y2 point will be sin * route_range
-        y2 = math.sin(currentAngle) * route_range;
+        y2 = math.sin(currentAngle) * route_range
 
-        newLat = lat+x2;
-        newLongit = longit+y2;
-        lat_long = (newLat,newLongit);
-        track_points.append(lat_long);
+        newLat = lat+x2
+        newLongit = longit+y2
+        lat_long = (newLat,newLongit)
+        track_points.append(lat_long)
 
         # Shift our angle around for the next point
-        currentAngle += degreesPerPoint;
+        currentAngle += degreesPerPoint
 
     idxi = randint(0,len(track_points)-2)
     idxf = len(track_points)-1
@@ -68,20 +68,14 @@ if __name__ == "__main__":
 
     init_lat = 40.714224
     init_lon = -73.961452
-
     route_range = 0.5
-    N_runs = 10;
 
-    data_stream = {"runs" : []}
-
-    for r in range(N_runs):
+    for r in range(10):
         rand_coord_i,rand_coord_f = findCoordinates(init_lat,init_lon,route_range)
 
-        directions_result = gmaps.directions(rand_coord_i, rand_coord_f ,mode="bicycling")
-        if not directions_result :
+        directions_result = gmaps.directions(rand_coord_i, rand_coord_f, mode="bicycling")
+        if not directions_result:
             continue
-
-        curr_run = {"steps" : []}
 
         duration, gps_route_key_points = run_steps(directions_result)
 
@@ -96,28 +90,23 @@ if __name__ == "__main__":
         sample_num = duration//10
 
         for i in range(sample_num):
-            data_inst = {}
+            step = {}
 
             curr_time = initial_datetime + timedelta(0,10*i)
-            data_inst["timestamp"] = curr_time.isoformat()
+            step["timestamp"] = curr_time.isoformat()
 
             curr_point = line.interpolate(line.length/sample_num*i)
-            data_inst["lat"] = curr_point.x
-            data_inst["lng"] = curr_point.y
+            step["lat"] = curr_point.x
+            step["lng"] = curr_point.y
 
             curr_co2 += randint(0,10) - 5
-            data_inst["co2"] = curr_co2
+            step["co2"] = curr_co2
 
             curr_temperature += (randint(0,24)-((curr_time.hour - 12)**2)**(1/2))/100
-            data_inst["temp"] = curr_temperature
-            curr_run["steps"].append(data_inst)
+            step["temp"] = curr_temperature
 
-        data_stream["runs"].append(curr_run)
-
-    for run in data_stream['runs']:
-        for step in run['steps']:
+            # Send it off to the broker
             print(step)
-
             producer = KafkaProducer(bootstrap_servers='kafka1:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
             producer.send('test', step)
             time.sleep(1)
