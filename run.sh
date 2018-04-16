c4606de16d0b0653068db3eadbf2fcb9c42210b4
xdye <dierduarte@gmail.com>
2018-04-16 17:57:40 +0100

    Updated setup script

--- run.sh
+++ run.sh
@@ -2,15 +2,23 @@
 set -eu
 
 docker-compose down --volumes
-docker-compose build
-docker-compose up -d zookeeper-1 zookeeper-2 zookeeper-3 kafka-1 kafka-2 kafka-3
+docker-compose up --build -d zookeeper-1 zookeeper-2 zookeeper-3 kafka-1 kafka-2 kafka-3
+
+# Enable monotoring
+if [ $# -ne 0 ]; then
+    case $1 in
+        -m|--monotoring)
+        docker-compose up -d elasticsearch kibana kibana_index_pattern logstash logspout metricbeat metricbeat-dashboard-setup
+        ;;
+        *)
+        # Default
+        ;;
+    esac
+fi
+
+echo "Waiting 60 sec for kafka to set up..."
 sleep 60
 docker-compose up -d websocket
 docker-compose up -d --scale sensor=10 sensor
 docker-compose up -d alerts
-case $1 in
-    -m|--monotoring)
-    docker-compose up -d elasticsearch kibana kibana_index_pattern logstash logspout metricbeat metricbeat-dashboard-setup
-    ;;
-esac
 docker-compose up -d --build client
