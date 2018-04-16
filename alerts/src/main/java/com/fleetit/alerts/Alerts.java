09dcd3fa1bfecb049c0cfd5c792bfb84b6587051
Fábio Maia <fabiu.maia@gmail.com>
2018-04-16 18:37:14 +0100

    Fix wip alerts

--- alerts/src/main/java/com/fleetit/alerts/Alerts.java
+++ alerts/src/main/java/com/fleetit/alerts/Alerts.java
@@ -1,25 +1,20 @@
 package com.fleetit.alerts;
 
-import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.utils.Bytes;
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.KeyValueMapper;
-import org.apache.kafka.streams.kstream.Materialized;
-import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.kstream.ValueMapper;
-import org.apache.kafka.streams.state.KeyValueStore;
 
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.Properties;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.streams.*;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
+
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
 
-public class WordCount {
-
-    public static void main(String[] args) throws Exception {
+public class Alerts {
+    public static void main(String[] args) {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "fleetit-alerts");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv("KAFKA_CLUSTER"));
@@ -27,13 +22,9 @@ public class WordCount {
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         final StreamsBuilder builder = new StreamsBuilder();
-
-        builder.<String, String>stream("updates")
-               .flatMapValues(value -> Arrays.asList(value.toLowerCase(Locale.getDefault()).split("\\W+")))
-               .groupBy((key, value) -> value)
-               .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("counts-store"))
-               .toStream()
-               .to("alerts", Produced.with(Serdes.String(), Serdes.Long()));
+        final KStream<String, String> updates = builder.stream("updates");
+        //final KStream<String, Update> speedAlerts = updates.filter((k, v) -> v.speed > 3);
+        updates.to("alerts-speed");
 
         final Topology topology = builder.build();
         final KafkaStreams streams = new KafkaStreams(topology, props);
@@ -54,6 +45,7 @@ public class WordCount {
         } catch (Throwable e) {
             System.exit(1);
         }
-        //System.exit(0);
+
+        System.exit(0);
     }
 }
