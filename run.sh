695094a021abd3ada56a6ab00ce3a5a4f8ae9e16
Fábio Maia <fabiu.maia@gmail.com>
2018-04-19 23:11:06 +0100

    Update run script

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 set -eu
 
 docker-compose down --volumes --remove-orphans
-docker-compose up -d --build zookeeper-1 zookeeper-2 zookeeper-3 kafka-1 kafka-2 kafka-3
+docker-compose build
+docker-compose up -d zookeeper-1 zookeeper-2 zookeeper-3 kafka-1 kafka-2 kafka-3
 
 # Enable monotoring
 if [ $# -ne 0 ]; then
@@ -18,7 +19,7 @@ fi
 
 echo "Waiting 60 sec for kafka to set up..."
 sleep 60
-docker-compose up -d --build websocket
-docker-compose up -d --build --scale sensor=2 sensor
-docker-compose up -d --build alerts
-docker-compose up -d --build client
+docker-compose up -d websocket
+docker-compose up -d --scale sensor=2 sensor
+docker-compose up -d alerts
+docker-compose up -d client
