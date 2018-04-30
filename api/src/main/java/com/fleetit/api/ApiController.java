ac8ad6561446598df93cb011a8f58cab6d8ed3b4
Fábio Maia <fabiu.maia@gmail.com>
2018-04-30 14:42:26 +0100

    Add global stats endpoint

--- api/src/main/java/com/fleetit/api/ApiController.java
+++ api/src/main/java/com/fleetit/api/ApiController.java
@@ -16,6 +16,26 @@ public class ApiController {
     private static final String POSTGRES_USER = System.getenv("POSTGRES_USER");
     private static final String POSTGRES_PASSWORD = System.getenv("POSTGRES_PASSWORD");
 
+    private static Update getGlobalStats() {
+        final String query = "SELECT now() AS timestamp, AVG(lat) AS lat, AVG(lng) AS lng, AVG(co2) AS co2, AVG(temp) AS temp, AVG(heart_rate) AS heart_rate, AVG(battery) AS battery, AVG(speed) AS speed FROM updates";
+        Update u = null;
+
+        try (Connection con = DriverManager.getConnection(POSTGRES_URL, POSTGRES_USER, POSTGRES_PASSWORD)) {
+            try (PreparedStatement pst = con.prepareStatement(query)) {
+                try (ResultSet rs = pst.executeQuery()) {
+                    while (rs.next()) {
+                        u = new Update(0, rs.getTimestamp("timestamp"), rs.getDouble("lat"), rs.getDouble("lng"), rs.getInt("co2"), rs.getDouble("temp"), rs.getInt("heart_rate"), rs.getDouble("battery"), rs.getDouble("speed"));
+                        System.out.println(u);
+                    }
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ApiController.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
+        }
+
+        return u;
+    }
+
     private static Update getBikeStats(int bikeId) {
         final String query = "SELECT bike_id, now() AS timestamp, AVG(lat) AS lat, AVG(lng) AS lng, AVG(co2) AS co2, AVG(temp) AS temp, AVG(heart_rate) AS heart_rate, AVG(battery) AS battery, AVG(speed) AS speed FROM updates WHERE bike_id = ? GROUP BY bike_id";
         Update u = null;
@@ -60,6 +80,11 @@ public class ApiController {
         return updates;
     }
 
+    @GetMapping("/stats")
+    @ResponseBody
+    public Update stats() {
+        return getGlobalStats();
+    }
     @GetMapping("/bike/{id}/stats")
     @ResponseBody
     public Update stats(@PathVariable("id") int id) {
--- api/test.sh
+++ api/test.sh
@@ -2,3 +2,5 @@
 curl -X GET -H "Accept: application/json" http://localhost:8080/api/v1/bike/8925/stats
 printf "\n\n"
 curl -X GET -H "Accept: application/json" http://localhost:8080/api/v1/bike/8925/last/30
+printf "\n\n"
+curl -X GET -H "Accept: application/json" http://localhost:8080/api/v1/stats
