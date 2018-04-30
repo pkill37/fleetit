9972583e229e3a2f351dbf1f56cf6f2644b5ce26
Fábio Maia <fabiu.maia@gmail.com>
2018-04-30 12:09:55 +0100

    Refactor postgres connection details

--- api/src/main/java/com/fleetit/api/ApiController.java
+++ api/src/main/java/com/fleetit/api/ApiController.java
@@ -12,22 +12,21 @@ import java.util.logging.Logger;
 @CrossOrigin
 @RequestMapping("/api/v1")
 public class ApiController {
-    // TODO: refactor to environment variables
-    private static final String POSTGRES_URL = "jdbc:postgresql://postgres/fleetit";
-    private static final String POSTGRES_USER = "root";
-    private static final String POSTGRES_PASS = "demo1234";
+    private static final String POSTGRES_URL = "jdbc:postgresql://" + System.getenv("POSTGRES_HOST") + "/" + System.getenv("POSTGRES_DB");
+    private static final String POSTGRES_USER = System.getenv("POSTGRES_USER");
+    private static final String POSTGRES_PASSWORD = System.getenv("POSTGRES_PASSWORD");
 
     private static Update getBikeStats(int bikeId) {
         final String query = "SELECT bike_id, now() AS timestamp, AVG(lat) AS lat, AVG(lng) AS lng, AVG(co2) AS co2, AVG(temp) AS temp, AVG(heart_rate) AS heart_rate, AVG(battery) AS battery, AVG(speed) AS speed FROM updates WHERE bike_id = ? GROUP BY bike_id";
         Update u = null;
 
-        try (Connection con = DriverManager.getConnection(POSTGRES_URL, POSTGRES_USER, POSTGRES_PASS)) {
+        try (Connection con = DriverManager.getConnection(POSTGRES_URL, POSTGRES_USER, POSTGRES_PASSWORD)) {
             try (PreparedStatement pst = con.prepareStatement(query)) {
                 pst.setInt(1, bikeId);
                 try (ResultSet rs = pst.executeQuery()) {
                     while (rs.next()) {
                         u = new Update(rs.getInt("bike_id"), rs.getTimestamp("timestamp"), rs.getDouble("lat"), rs.getDouble("lng"), rs.getInt("co2"), rs.getDouble("temp"), rs.getInt("heart_rate"), rs.getDouble("battery"), rs.getDouble("speed"));
-                        System.out.print(u);
+                        System.out.println(u);
                     }
                 }
             }
@@ -42,14 +41,14 @@ public class ApiController {
         final String query = "SELECT * FROM updates WHERE bike_id = ? ORDER BY timestamp LIMIT ?";
         List<Update> updates = new ArrayList<>();
 
-        try (Connection con = DriverManager.getConnection(POSTGRES_URL, POSTGRES_USER, POSTGRES_PASS)) {
+        try (Connection con = DriverManager.getConnection(POSTGRES_URL, POSTGRES_USER, POSTGRES_PASSWORD)) {
             try (PreparedStatement pst = con.prepareStatement(query)) {
                 pst.setInt(1, bikeId);
                 pst.setInt(2, days);
                 try (ResultSet rs = pst.executeQuery()) {
                     while (rs.next()) {
                         Update u = new Update(rs.getInt("bike_id"), rs.getTimestamp("timestamp"), rs.getDouble("lat"), rs.getDouble("lng"), rs.getInt("co2"), rs.getDouble("temp"), rs.getInt("heart_rate"), rs.getDouble("battery"), rs.getDouble("speed"));
-                        System.out.print(u);
+                        System.out.println(u);
                         updates.add(u);
                     }
                 }
