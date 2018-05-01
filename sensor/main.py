0bd45970c72ad464fd654c1638b988caa5b1a825
Fábio Maia <fabiu.maia@gmail.com>
2018-05-01 22:44:27 +0100

    Fix

--- sensor/main.py
+++ sensor/main.py
@@ -3,27 +3,32 @@ import json
 import dateutil.parser
 import time
 import datetime
+import random
 import os
 
 producer = KafkaProducer(bootstrap_servers=os.environ['KAFKA_CLUSTER'], value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
+all_runs = []
 with open("runs.json", "r") as f:
-    # line will have up to 50 runs max
     for line in f:
         runs = json.loads(line)
+        if runs:
+            for run in runs:
+                all_runs.append(run)
 
-        #actual run to send to sensor
-        #difference between now and sensor timestamp
-        for run in runs:
-            timestamp_delta = datetime.datetime.now() - dateutil.parser.parse(run[0]["timestamp"])
 
-            sampling_times = [(dateutil.parser.parse(run[step_idx+1]["timestamp"]) - dateutil.parser.parse(run[step_idx]["timestamp"])).seconds for step_idx in range(len(run)-1)]
-            sampling_times.append(0)
-            for i in range(len(run)):
-                # update timestamp
-                run[i]["timestamp"] = (dateutil.parser.parse(run[i]["timestamp"]) + timestamp_delta).strftime("%Y-%m-%d %H:%M:%S")
+while True:
+    #actual run to send to sensor
+    #difference between now and sensor timestamp
+    run = all_runs[random.randint(0,len(all_runs)-1)]
+    timestamp_delta = datetime.datetime.now() - dateutil.parser.parse(run[0]["timestamp"])
 
-                print(run[i])
-                producer.send('updates', run[i])
-                time.sleep(sampling_times[i])
+    sampling_times = [(dateutil.parser.parse(run[step_idx+1]["timestamp"]) - dateutil.parser.parse(run[step_idx]["timestamp"])).seconds for step_idx in range(len(run)-1)]
+    sampling_times.append(0)
+    for i in range(len(run)):
+        #update timestamp
+        run[i]["timestamp"] = (dateutil.parser.parse(run[i]["timestamp"]) + timestamp_delta).strftime("%Y-%m-%d %H:%M:%S")
+
+        producer.send('updates', run[i])
+        time.sleep(sampling_times[i])
 
