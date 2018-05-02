39884b68f5bdfb2e6650bda87dd11e075fcd82ae
Fábio Maia <fabiu.maia@gmail.com>
2018-05-02 01:45:42 +0100

    Add chart

--- client/src/Detail.js
+++ client/src/Detail.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 
 const API_URL = 'http://localhost:8080/api/v1'
 
 class Detail extends React.Component {
   state = {
     stats: null,
-    last: null
+    last: []
   }
 
   async componentDidMount() {
@@ -21,20 +22,37 @@ class Detail extends React.Component {
   render() {
     return (
       <div className="content-wrapper">
-        {this.state.stats != null &&
-          <div>
-            <h1>Bike #{this.state.stats.bikeId} Most Recent Details</h1>
-            <ul>
-              <li>Speed: {this.state.stats.speed} km/h</li>
-              <li>CO2: {this.state.stats.co2}</li>
-              <li>Heart Rate: {this.state.stats.heartRate}</li>
-              <li>Temperature: {this.state.stats.temp}</li>
-              <li>Battery: {this.state.stats.battery}</li>
-              <li>Latitude: {this.state.stats.lat}</li>
-              <li>Longitude: {this.state.stats.lng}</li>
-            </ul>
-          </div>
-        }
+      {this.state.stats !== null &&
+        <div>
+        <h1>Bike #{this.state.stats.bikeId} Most Recent Details</h1>
+        <ul>
+        <li>Speed: {this.state.stats.speed} km/h</li>
+        <li>CO2: {this.state.stats.co2}</li>
+        <li>Heart Rate: {this.state.stats.heartRate}</li>
+        <li>Temperature: {this.state.stats.temp}</li>
+        <li>Battery: {this.state.stats.battery}</li>
+        <li>Latitude: {this.state.stats.lat}</li>
+        <li>Longitude: {this.state.stats.lng}</li>
+        </ul>
+        </div>
+      }
+
+      {this.state.last.length !== 0 &&
+          <ResponsiveContainer minHeight={300}>
+            <LineChart data={this.state.last}>
+              <XAxis ticks={false} dataKey="timestamp"/>
+              <YAxis/>
+              <Tooltip/>
+              <Legend />
+              <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+              <Line type="monotone" dataKey="heartRate" stroke="#8884d8" />
+              <Line type="monotone" dataKey="battery" stroke="#82ca9d" />
+              <Line type="monotone" dataKey="speed" stroke="#ff7f0e" />
+              <Line type="monotone" dataKey="co2" stroke="#bcbd22" />
+              <Line type="monotone" dataKey="temp" stroke="#e377c2" />
+            </LineChart>
+          </ResponsiveContainer>
+      }
       </div>
     )
   }
