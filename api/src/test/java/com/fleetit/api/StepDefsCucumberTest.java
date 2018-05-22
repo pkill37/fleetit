9cbfd5d4eb7d36a88c513178a89f7f2e5123d298
Fábio Maia <fabiu.maia@gmail.com>
2018-05-22 22:13:00 +0100

    Fix BDD tests

--- api/src/test/java/com/fleetit/api/StepDefsCucumberTest.java
+++ api/src/test/java/com/fleetit/api/StepDefsCucumberTest.java
@@ -13,14 +13,13 @@ public class StepDefsCucumberTest extends SpringCucumberTest{
 	public void the_client_issues_GET_version() throws Throwable{
 	    executeGet("http://127.0.0.1:8080/api/v1/stats");
 	}
-	 
+
 	@Then("^the client receives status code of (\\d+)$")
 	public void the_client_receives_status_code_of(int statusCode) throws Throwable {
 	    HttpStatus currentStatusCode = latestResponse.getTheResponse().getStatusCode();
-	    assertThat("status code is incorrect : "+ 
-	    latestResponse.getBody(), currentStatusCode.value(), is(statusCode));
+	    assertThat("status code is incorrect : "+ latestResponse.getBody(), currentStatusCode.value(), is(statusCode));
 	}
-	 
+
 	@And("^the client receives the stats$")
 	public void the_client_receives_server_version_body() throws Throwable {
 	    assertThat(latestResponse.getBody(), notNullValue());
@@ -28,21 +27,31 @@ public class StepDefsCucumberTest extends SpringCucumberTest{
 
 	@When("^the client calls /api/v1/bike/(\\d+)/stats$")
 	public void the_client_issues_GET_version(int id) throws Throwable{
-	    executeGet("http://127.0.0.1:8080/api/v1/"+id+"/stats");
+	    executeGet("http://127.0.0.1:8080/api/v1/bike/"+id+"/stats");
 	}
-	 
-	@And("^the client receives the stats of bike (\\d+)$")
+
+	@Then("^the client receives the stats of bike (\\d+)$")
 	public void the_client_receives_server_version_body(int id) throws Throwable {
 	    assertThat(latestResponse.getBody(), notNullValue());
 	}
 
+    @And("^the client receives the stats of bike$")
+    public void the_client_receives_the_stats_of_bike() throws Throwable {
+	    assertThat(latestResponse.getBody(), notNullValue());
+    }
+
 	@When("^the client calls /api/v1/bike/(\\d+)/last/(\\d+)$")
 	public void the_client_issues_GET_version(int id, int days) throws Throwable{
-	    executeGet("http://127.0.0.1:8080/api/v1/"+id+"/last/"+days);
+	    executeGet("http://127.0.0.1:8080/api/v1/bike/"+id+"/last/"+days);
 	}
-	 
-	@And("^the client receives the stats from the last (\\d+) days of bike (\\d+)$")
+
+	@Then("^the client receives the stats from the last (\\d+) days of bike (\\d+)$")
 	public void the_client_receives_server_version_body(int days, int id) throws Throwable {
 	    assertThat(latestResponse.getBody(), notNullValue());
 	}
+
+    @And("^the client receives the stats from the last (\\d+) days of bike$")
+    public void the_client_receives_the_stats_from_the_last_days_of_bike(int arg1) throws Throwable {
+	    assertThat(latestResponse.getBody(), notNullValue());
+    }
 }
