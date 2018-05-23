74052bac202d13074d649c2dbf29d38f3d9871a5
Fábio Maia <fabiu.maia@gmail.com>
2018-05-23 12:19:46 +0100

    Update BDD tests

--- api/src/test/java/com/fleetit/api/StepDefsCucumberTest.java
+++ api/src/test/java/com/fleetit/api/StepDefsCucumberTest.java
@@ -10,48 +10,50 @@ import org.springframework.http.HttpStatus;
 
 public class StepDefsCucumberTest extends SpringCucumberTest{
 	@When("^the client calls /api/v1/stats$")
-	public void the_client_issues_GET_version() throws Throwable{
+	public void the_client_issues_GET_to_stats() throws Throwable{
 	    executeGet("http://127.0.0.1:8080/api/v1/stats");
 	}
 
-	@Then("^the client receives status code of (\\d+)$")
-	public void the_client_receives_status_code_of(int statusCode) throws Throwable {
+	@Then("^the client receives status code at stats of (\\d+)$")
+	public void the_client_receives_status_code_of_stats(int statusCode) throws Throwable {
 	    HttpStatus currentStatusCode = latestResponse.getTheResponse().getStatusCode();
-	    assertThat("status code is incorrect : "+ latestResponse.getBody(), currentStatusCode.value(), is(statusCode));
+	    assertThat("status code is correct", currentStatusCode.value(), is(statusCode));
 	}
 
 	@And("^the client receives the stats$")
-	public void the_client_receives_server_version_body() throws Throwable {
+	public void the_client_receives_stats() throws Throwable {
 	    assertThat(latestResponse.getBody(), notNullValue());
 	}
 
 	@When("^the client calls /api/v1/bike/(\\d+)/stats$")
-	public void the_client_issues_GET_version(int id) throws Throwable{
+	public void the_client_issues_GET_to_bike_stats(int id) throws Throwable{
 	    executeGet("http://127.0.0.1:8080/api/v1/bike/"+id+"/stats");
 	}
 
-	@Then("^the client receives the stats of bike (\\d+)$")
-	public void the_client_receives_server_version_body(int id) throws Throwable {
+    @Then("^the client receives status code at stats of bike of (\\d+)$")
+    public void the_client_receives_status_code_of_stats_of_bike(int statusCode) throws Throwable {
+	    HttpStatus currentStatusCode = latestResponse.getTheResponse().getStatusCode();
+	    assertThat("status code is correct", currentStatusCode.value(), is(statusCode));
+    }
+
+    @And("^the client receives the stats of bike (\\d+)$")
+	public void the_client_receives_the_stats_of_bike(int id) throws Throwable {
 	    assertThat(latestResponse.getBody(), notNullValue());
 	}
 
-    @And("^the client receives the stats of bike$")
-    public void the_client_receives_the_stats_of_bike() throws Throwable {
-	    assertThat(latestResponse.getBody(), notNullValue());
-    }
-
 	@When("^the client calls /api/v1/bike/(\\d+)/last/(\\d+)$")
-	public void the_client_issues_GET_version(int id, int days) throws Throwable{
+	public void the_client_issues_GET_to_last_days_of_bike(int id, int days) throws Throwable{
 	    executeGet("http://127.0.0.1:8080/api/v1/bike/"+id+"/last/"+days);
 	}
 
-	@Then("^the client receives the stats from the last (\\d+) days of bike (\\d+)$")
-	public void the_client_receives_server_version_body(int days, int id) throws Throwable {
+    @Then("^the client receives status code at last days of bike of (\\d+)$")
+    public void the_client_receives_status_code_of_last_days_of_bike(int statusCode) throws Throwable {
+	    HttpStatus currentStatusCode = latestResponse.getTheResponse().getStatusCode();
+	    assertThat("status code is correct", currentStatusCode.value(), is(statusCode));
+    }
+
+	@And("^the client receives the stats from the last (\\d+) days of bike (\\d+)$")
+	public void the_client_receives_the_stats_from_the_last_days_of_bike(int days, int id) throws Throwable {
 	    assertThat(latestResponse.getBody(), notNullValue());
 	}
-
-    @And("^the client receives the stats from the last (\\d+) days of bike$")
-    public void the_client_receives_the_stats_from_the_last_days_of_bike(int arg1) throws Throwable {
-	    assertThat(latestResponse.getBody(), notNullValue());
-    }
 }
