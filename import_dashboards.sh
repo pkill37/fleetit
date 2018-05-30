ad7945b900044c19a7a50512e2de1ef00143492a
Fábio Maia <fabiu.maia@gmail.com>
2018-05-30 11:58:31 +0100

    Fix

--- import_dashboards.sh
+++ import_dashboards.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+set -euo pipefail
+
 KIBANA_URL="http://127.0.0.1:5601"
 DASHBOARD_PATH="./monitoring/customDashboards"
 
 
 # Import searches
 for sch in $DASHBOARD_PATH/searches/*; do
-    ./customDashboards/kibana-importer/kibana-importer.py --json $sch --kibana-url $KIBANA_URL
+    $DASHBOARD_PATH/kibana-importer/kibana-importer.py --json $sch --kibana-url $KIBANA_URL
 done
 
 # Import visualizations
 for vis in $DASHBOARD_PATH/visualizations/*; do
-    ./customDashboards/kibana-importer/kibana-importer.py --json $vis --kibana-url $KIBANA_URL
+    $DASHBOARD_PATH/kibana-importer/kibana-importer.py --json $vis --kibana-url $KIBANA_URL
 done
 
 # Import dashboards
 for dash in $DASHBOARD_PATH/dashboards/*; do
-    ./customDashboards/kibana-importer/kibana-importer.py --json $dash --kibana-url $KIBANA_URL
+    $DASHBOARD_PATH/kibana-importer/kibana-importer.py --json $dash --kibana-url $KIBANA_URL
 done
