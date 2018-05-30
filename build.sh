c60c798055ec4c405de9d187a14ae9d23e8d9f9b
Fábio Maia <fabiu.maia@gmail.com>
2018-05-30 11:24:02 +0100

    Hotfix

--- build.sh
+++ build.sh
@@ -2,35 +2,14 @@
 
 set -euo pipefail
 
-docker build -t faviouz/fleetit-jenkins jenkins
-docker push faviouz/fleetit-jenkins:latest
-
-docker build -t faviouz/fleetit-sensor sensor
-docker push faviouz/fleetit-sensor:latest
-
-docker build -t faviouz/fleetit-websocket websocket
-docker push faviouz/fleetit-websocket:latest
-
-docker build -t faviouz/fleetit-client-development -f client/Dockerfile.development client
-docker push faviouz/fleetit-client-development:latest
-
-docker build -t faviouz/fleetit-client-production -f client/Dockerfile.production client
-docker push faviouz/fleetit-client-production:latest
-
-docker build -t faviouz/fleetit-postgres postgres
-docker push faviouz/fleetit-postgres:latest
-
-docker build -t faviouz/fleetit-api api
-docker push faviouz/fleetit-api:latest
-
-docker build -t faviouz/fleetit-alerts alerts
-docker push faviouz/fleetit-alerts:latest
-
-docker build -t faviouz/fleetit-elasticsearch monitoring/elasticsearch
-docker push faviouz/fleetit-elasticsearch:latest
-
-docker build -t faviouz/fleetit-metricbeat monitoring/metricbeat
-docker push faviouz/fleetit-metricbeat:latest
-
-docker build -t faviouz/fleetit-kibana monitoring/kibana
-docker push faviouz/fleetit-kibana:latest
+docker build -t fleetit-jenkins jenkins
+docker build -t fleetit-sensor sensor
+docker build -t fleetit-websocket websocket
+docker build -t fleetit-client-development -f client/Dockerfile.development client
+docker build -t fleetit-client-production -f client/Dockerfile.production client
+docker build -t fleetit-postgres postgres
+docker build -t fleetit-api api
+docker build -t fleetit-alerts alerts
+docker build -t fleetit-elasticsearch monitoring/elasticsearch
+docker build -t fleetit-metricbeat monitoring/metricbeat
+docker build -t fleetit-kibana monitoring/kibana
