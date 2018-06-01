23473cfc9a00db66c9afd7e21fca688795af912c
Fábio Maia <fabiu.maia@gmail.com>
2018-06-01 01:52:19 +0100

    Add missing stats units

--- client/src/Stats.js
+++ client/src/Stats.js
@@ -21,34 +21,34 @@ class Stats extends React.Component {
             <h1>Stats</h1>
             {this.state.stats != null && (
                 <div className="card-deck">
-                    <div className="card text-white bg-danger mb-3" style={{maxWidth: '12rem'}}>
+                    <div className="card text-white bg-danger mb-3" style={{maxWidth: '18rem'}}>
                         <div className="card-header">Average Heart Rate</div>
                         <div className="card-body">
-                            <h1 className="card-title text-center">{this.state.stats.heartRate.toFixed(2)}</h1>
+                            <h1 className="card-title text-center">{this.state.stats.heartRate.toFixed(2)} bps</h1>
                         </div>
                     </div>
-                    <div className="card text-white bg-warning mb-3" style={{maxWidth: '12rem'}}>
+                    <div className="card text-white bg-warning mb-3" style={{maxWidth: '18rem'}}>
                         <div className="card-header">Average Battery</div>
                         <div className="card-body">
-                            <h1 className="card-title text-center">{this.state.stats.battery.toFixed(2)}</h1>
+                            <h1 className="card-title text-center">{this.state.stats.battery.toFixed(2)}%</h1>
                         </div>
                     </div>
-                    <div className="card text-white bg-success mb-3" style={{maxWidth: '12rem'}}>
+                    <div className="card text-white bg-success mb-3" style={{maxWidth: '18rem'}}>
                         <div className="card-header">Average Speed</div>
                         <div className="card-body">
-                            <h1 className="card-title text-center">{this.state.stats.speed.toFixed(2)}</h1>
+                            <h1 className="card-title text-center">{this.state.stats.speed.toFixed(2)} km/h</h1>
                         </div>
                     </div>
-                    <div className="card text-white bg-primary mb-3" style={{maxWidth: '13rem'}}>
+                    <div className="card text-white bg-primary mb-3" style={{maxWidth: '18rem'}}>
                         <div className="card-header">Average Temperature</div>
                         <div className="card-body">
-                            <h1 className="card-title text-center">{this.state.stats.temp.toFixed(2)}</h1>
+                            <h1 className="card-title text-center">{this.state.stats.temp.toFixed(2)} ºC</h1>
                         </div>
                     </div>
-                    <div className="card text-white bg-dark mb-3" style={{maxWidth: '12rem'}}>
+                    <div className="card text-white bg-dark mb-3" style={{maxWidth: '18rem'}}>
                         <div className="card-header">Average CO2</div>
                         <div className="card-body">
-                            <h1 className="card-title text-center">{this.state.stats.co2.toFixed(2)}</h1>
+                            <h1 className="card-title text-center">{this.state.stats.co2.toFixed(2)} ppm</h1>
                         </div>
                     </div>
                 </div>
