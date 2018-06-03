e3645a10c6ee85557a0f458ac4bf0b8252283afc
Fábio Maia <fabiu.maia@gmail.com>
2018-06-03 21:07:49 +0100

    Remove intermediate containers on build

--- build.sh
+++ build.sh
@@ -2,26 +2,26 @@
 
 set -euo pipefail
 
-docker build -t fleetit-jenkins jenkins
+docker build --rm -t fleetit-jenkins jenkins
 
-docker build -t fleetit-sensor sensor
+docker build --rm -t fleetit-sensor sensor
 
-docker build -t fleetit-websocket websocket
+docker build --rm -t fleetit-websocket websocket
 
-docker build -t fleetit-client-development -f client/Dockerfile.development client
+docker build --rm -t fleetit-client-development -f client/Dockerfile.development client
 
-docker build -t fleetit-client-production -f client/Dockerfile.production client
+docker build --rm -t fleetit-client-production -f client/Dockerfile.production client
 
-docker build -t fleetit-postgres postgres
+docker build --rm -t fleetit-postgres postgres
 
-docker build -t fleetit-api api
+docker build --rm -t fleetit-api api
 
-docker build -t fleetit-alerts alerts
+docker build --rm -t fleetit-alerts alerts
 
-docker build -t fleetit-elasticsearch monitoring/elasticsearch
+docker build --rm -t fleetit-elasticsearch monitoring/elasticsearch
 
-docker build -t fleetit-metricbeat monitoring/metricbeat
+docker build --rm -t fleetit-metricbeat monitoring/metricbeat
 
-docker build -t fleetit-kibana monitoring/kibana
+docker build --rm -t fleetit-kibana monitoring/kibana
 
-docker build -t fleetit-import_dashboards monitoring/customDashboards
+docker build --rm -t fleetit-import_dashboards monitoring/customDashboards
